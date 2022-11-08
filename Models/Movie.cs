0bf06a9756bccbb4d7461cd96ababa55737f6ea1
rorikwong <108846354+rorikwong@users.noreply.github.com>
2022-11-08 15:57:59 -0600

    combining attributes

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,17 +7,23 @@ namespace RazorPagesMovies.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; } = string.Empty;
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date") , DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
         public string Genre { get; set; } = string.Empty;
 
+        [Range(1, 100) , DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), StringLength(5)]
         public string Rating { get; set; } = string.Empty;
 
-        
+
     }
 }
