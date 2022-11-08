4535588e778d5e05c1005ff940356b37af0c9e63
rorikwong <108846354+rorikwong@users.noreply.github.com>
2022-11-08 13:16:47 -0600

    adding rating class to create.cshtml

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovies.Data;
 using RazorPagesMovies.Models;
@@ -20,13 +21,27 @@ namespace RazorPagesMovies.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string ? SearchString { get; set; }
+        public SelectList ? Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            if (_context.Movie != null)
+            // use LINQ to get a list of movie genres
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
             {
-                Movie = await _context.Movie.ToListAsync();
+                movies = movies.Where(x => x.Genre == MovieGenre);
             }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
