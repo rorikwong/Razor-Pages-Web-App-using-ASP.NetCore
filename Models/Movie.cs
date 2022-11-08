2e64518cdd1cdcee497c38011070e957d97f5d24
rorikwong <108846354+rorikwong@users.noreply.github.com>
2022-11-07 22:26:45 -0600

    adding Model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,6 +1,15 @@
-﻿namespace RazorPagesMovies.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovies.Models
 {
     public class Movie
     {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
     }
 }
